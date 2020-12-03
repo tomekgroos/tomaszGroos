22e56d1055cd21f6e45fb2dc24d521718f95abdc
Your Name <tomek.groos@gmail.com>
2020-12-04 00:36:15 +0100

    added parallax for header

---
+++ header.js
@@ -0,0 +1,11 @@
+const fade = document.querySelectorAll(".fade");
+
+window.addEventListener("scroll", ()=>{
+    let scroll = window.pageYOffset;
+    
+    fade.forEach(el => {
+        let velocity = el.dataset.velocity;
+        el.style.transform = `translateY(${scroll * velocity}px)`
+
+    })
+})
